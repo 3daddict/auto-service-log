557a3ad6276d9c00094d50d0b1594a40d0355d6d
3DAddict <print@3daddict.com>
2019-01-24 11:32:57 -0800

    redux form with reactstrap input onSubmit

--- client/src/components/authentication/SignUp.js
+++ client/src/components/authentication/SignUp.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import { Container, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Container, Form, FormGroup, Label } from 'reactstrap';
 
 class SignUp extends Component {
+  onSubmit = formProps => {
+    console.log(formProps);
+  };
 
   render() {
+    const { handleSubmit } = this.props;
     return (
       <Container>
-      <Form>
+      <Form onSubmit={ handleSubmit(this.onSubmit) }>
         <FormGroup>
           <Label for="signUpEmail">Email</Label>
           <Field 
+            className="form-control"
             name="email"
             type="text"
-            component={Input}
+            component="input"
             id="signUpEmail"
             placeholder="Enter an email"
             autoComplete="off"
@@ -22,14 +27,18 @@ class SignUp extends Component {
         <FormGroup>
           <Label for="signUpPassword">Password</Label>
           <Field 
+          className="form-control"
             name="password"
             type="password"
-            component={Input}
+            component="input"
             id="signUpPassword" 
             placeholder="Enter a password"
             autoComplete="off"
           />
         </FormGroup>
+        <div className="text-right">
+          <Button className="mb-2" color="primary">Sign Up</Button>
+        </div>
     </Form>
       </Container>
     )
